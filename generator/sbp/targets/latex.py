3fae73e470ae539153e9baea79f766c0f0c5aa3f
Mark Fine <mark.fine@gmail.com>
2015-03-24 00:47:58 -0700

    baseline.

---
+++ generator/sbp/targets/latex.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# Copyright (C) 2015 Swift Navigation Inc.
+# Contact: Bhaskar Mookerji <mookerji@swiftnav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+import re
+from sbp.targets.templating import *
+
+TEMPLATE_NAME = "sbp_messages_desc.tex"
+
+LATEX_SUBS = (
+    (re.compile(r'\\'), r'\\textbackslash'),
+    (re.compile(r'([{}_#%&$])'), r'\\\1'),
+    (re.compile(r'~'), r'\~{}'),
+    (re.compile(r'\^'), r'\^{}'),
+    (re.compile(r'_'), r'_'),
+    (re.compile(r'"'), r"''"),
+    (re.compile(r'\.\.\.+'), r'\\ldots'),
+)
+
+def escape_tex(value):
+    newval = value
+    for pattern, replacement in LATEX_SUBS:
+        newval = pattern.sub(replacement, newval)
+    return newval
+
+def classnameify(s):
+  return ''.join(w if w in ACRONYMS else w.title() for w in s.split('_'))
+
+def commentify(value):
+  return '\n'.join([' * ' + l for l in value.split('\n')[:-1]])
+
+JENV.filters['escape_tex'] = escape_tex
+JENV.filters['classnameify'] = classnameify
+JENV.filters['commentify'] = commentify
+
+def render_source(output_dir, package_specs):
+  """
+  Render and output
+  """
+  destination_filename = "./sbp/targets/resources/sbp_out.tex"
+  py_template = JENV.get_template(TEMPLATE_NAME)
+  with open(destination_filename, 'w') as f:
+    f.write(py_template.render(pkgs=package_specs))
+
